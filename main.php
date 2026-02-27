513400bc78ce9bf88125d341ecd25eae25514c01
Ddilibe <franklinfidelugwuowo@gmail.com>
2026-02-27 20:12:44 +0100

    feat: commented file path to activate plugin

--- main.php
+++ main.php
@@ -14,11 +14,11 @@ if (!defined('ABSPATH')) {
 define('MAIN_PATH', plugin_dir_path(__FILE__));
 
 // Database Initialization
-require_once plugin_dir_path(__FILE__) . 'src/database.php';
-register_activation_hook(__FILE__, 'nsa_create_registrations_table');
+// require_once plugin_dir_path(__FILE__) . 'src/database.php';
+// register_activation_hook(__FILE__, 'nsa_create_registrations_table');
 
-// Database Function Initialization
-require_once MAIN_PATH . 'src/db/conference.php';
+// // Database Function Initialization
+// require_once MAIN_PATH . 'src/db/conference.php';
 
 // Include required files
 // require_once MAIN_PATH . 'src/PRS/corporate.php';
