157989db3f7e0d263eb74b969a746ca877af82f2
Ddilibe <franklinfidelugwuowo@gmail.com>
2026-03-13 22:59:46 +0100

    fix: removed primary conditional

--- src/menu.php
+++ src/menu.php
@@ -4,7 +4,7 @@ add_filter('wp_nav_menu_items', 'filter_menu_by_permission', 10, 2);
 
 function filter_menu_by_permission($items, $args)
 {
-    if ($args->theme_location == 'primary') {
+    // if ($args->theme_location == 'primary') {
 
         if (current_user_can('manage_options')) {
             $items .= '<li class="menu-item"><a href="https://my.cison.org.ng/members/wp-admin/admin.php">Backend</a></li>';
@@ -14,7 +14,7 @@ function filter_menu_by_permission($items, $args)
         // if (in_array('journal_editor', (array) $user->roles)) {
         //     $items .= '<li class="menu-item"><a href="/editor-panel/">Review Manuscripts</a></li>';
         // }
-    }
+    // }
     return $items;
 }
 ?>
