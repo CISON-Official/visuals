2cf35d026d3a9881439e11bc0b938c2d0df77161
Ddilibe <franklinfidelugwuowo@gmail.com>
2026-04-07 11:56:13 +0100

    fix: added the uitstanding argument in bp user query uid clause function

--- src/corporate/default_member_dir.php
+++ src/corporate/default_member_dir.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * Exclude corporate users from the buddyboss members directory
+ * Exclude corporate users from the BuddyBoss members directory
  */
-
 add_action('bp_user_query_uid_clauses', function ($sql_clauses, $bp_user_query) {
     global $wpdb;
 
@@ -11,8 +10,8 @@ add_action('bp_user_query_uid_clauses', function ($sql_clauses, $bp_user_query) 
         SELECT user_id
         FROM {$wpdb->prefix}bp_xprofile_data
         WHERE field_id = %d
-        and value != %s
-    ", 1614, 'Corporate'));
+        AND value != %s
+    ", 1618, 'Corporate'));
 
     if (!empty($corporate_ids)) {
         $ids = implode(',', array_map('intval', $corporate_ids));
@@ -20,26 +19,22 @@ add_action('bp_user_query_uid_clauses', function ($sql_clauses, $bp_user_query) 
     }
 
     return $sql_clauses;
-});
+
+}, 10, 2); // <-- this was missing
 
 
 /**
- * Fix the member count to exclude Corporate Ueser
+ * Fix the member count to exclude Corporate users
  */
 add_filter('bp_get_total_member_count', function ($count) {
     global $wpdb;
 
-    if (!bp_is_members_directory()) {
-        return $count;
-    }
-
     $corporate_count = $wpdb->get_var($wpdb->prepare("
-        SELECT COUNT(*)
+        SELECT COUNT(DISTINCT user_id)
         FROM {$wpdb->prefix}bp_xprofile_data
         WHERE field_id = %d
-        and value != %s
-    ", 1614, 'Corporate'));
+        AND value != %s
+    ", 1618, 'Corporate'));
 
     return max(0, $count - intval($corporate_count));
 });
-
